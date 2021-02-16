f074d3f2132bac5200485c0cfd32ce80d178ebd0
Joris Hens <joris@goodbytes.be>
2021-02-16 17:06:01 +0100

    Added starter for lab1. Good luck students

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 2imd-php-lab1-starter
+## Bekijk de video instructies op Canvas voor een duidelijke beschrijving van dit labo
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    /*
+        todo1: maak een multidimensionale array met daarin alle checkins zoals te zien op screenshots/screenshot1.png
+            - denk na over welke data er in je array moet zitten
+            - soms voeg je een foto toe, soms niet (tip: gebruik voor je foto's pexels.com of een andere gratis leverancier)
+            - op screenshots/screenshot2.jpeg kan je zien wat bedoelt wordt met een checkin met foto
+            - werk met isset() of empty() om de foto soms wel en soms niet af te drukken
+
+
+        todo3: werk met een constant DISTANCE waarmee je kan instellen wat de maximale afstand is om checkins voor te tonen
+            - je zal in je array een extra stukje data moeten bijvoegen om deze afstand mee te betrekken in je checkins
+
+    */
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Swarm App</title>
+</head>
+<body>
+    <?php // todo2 : lus over je checkins en print deze visueel af zoals op de screenshots/screenshot1.png?>
+</body>
+</html>
